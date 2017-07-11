91250a3eb973e0a29f19083f2dee39bb26bfb898
luolibin <luolibin@pingcap.com>
2017-07-11 20:16:09 +0800

    add build groovy

--- pingcap_tidb_operator_build.groovy
+++ pingcap_tidb_operator_build.groovy
@@ -1,6 +1,6 @@
 def call(TIDB_OPERATOR_BRANCH){
   podTemplate(label: 'mypod-e2e-operator', containers: [
-    containerTemplate(name: 'k8s-operator-e2e', image: 'localhost:5000/pingcap/tidb-operator-e2e:1df176f-2017-07-05_06-20-16', ttyEnabled: true, command: '/usr/local/bin/e2e.test' args: '-ginkgo.v --operator-image=localhost:5000/pingcap/tidb-operator:latest --operator-service-ip=10.233.2.135'),
+    containerTemplate(name: 'k8s-operator-e2e', image: 'localhost:5000/pingcap/tidb-operator-e2e:1df176f-2017-07-05_06-20-16', ttyEnabled: true, command: '/usr/local/bin/e2e.test', args: '-ginkgo.v --operator-image=localhost:5000/pingcap/tidb-operator:latest --operator-service-ip=10.233.2.135')
   ]) {
 
     node('mypod-e2e-operator') {
