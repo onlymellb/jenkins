b332720b910f2602c1fb96f37a233de87f95df29
luolibin <luolibin@pingcap.com>
2017-07-24 17:46:00 +0800

    add cloud manager build scripts

---
+++ pingcap_tidb_cloud_manager_build.groovy
@@ -0,0 +1,75 @@
+def call(BUILD_BRANCH) {
+	
+	def GITHASH
+	env.GOPATH = "/go"
+	env.GOROOT = "/usr/local/go"
+	env.PATH = "${env.GOROOT}/bin:/bin:${env.PATH}"
+	def UCLOUD_OSS_URL = "http://pingcap-dev.hk.ufileos.com"
+	def BUILD_URL = "git@github.com:pingcap/tidb-cloud-manager.git"
+
+	//define k8s pod template
+	podTemplate(
+		label: 'centos7_build',
+		containers: [
+			containerTemplate(
+				name: 'build-env',
+				image: 'localhost:5000/pingcap/build_env:latest',
+				alwaysPullImage: true,
+				ttyEnabled: true,
+				command: 'cat')
+		]){
+		catchError {
+			node('centos7_build') {
+				def WORKSPACE = pwd()
+				stage('Build') {
+					dir("${WORKSPACE}/go/src/github.com/pingcap/tidb-cloud-manager"){
+						container('build-env') {
+							stage('build tidb-cloud-manager binary'){
+								// checkout source code
+								git credentialsId: "k8s", url: "${BUILD_URL}", branch: "${BUILD_BRANCH}"
+								GITHASH = sh(returnStdout: true, script: "git rev-parse HEAD").trim()
+
+								// build
+								sh "export GOPATH=${WORKSPACE}/go:$GOPATH && make"
+							}
+
+							stage('upload tidb-cloud-manager binary'){
+
+								//upload binary
+								sh """
+								cp /usr/local/bin/config.cfg ./
+								tar zcvf tidb-cloud-manager.tar.gz bin/* docker/*.tmpl
+								filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key builds/pingcap/cloud-manager/${GITHASH}/centos7/tidb-cloud-manager.tar.gz --file tidb-cloud-manager.tar.gz
+								"""
+
+								//update refs
+								writeFile file: 'sha1', text: "${GITHASH}"
+								sh "filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key refs/pingcap/cloud-manager/${BUILD_BRANCH}/centos7/sha1 --file sha1"
+							}
+						}
+					}
+				}
+			}
+			currentBuild.result = "SUCCESS"
+		}
+		stage('Summary') {
+			echo("echo summary info ########")
+			def DURATION = (((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60) as double).round(2)
+			def slackmsg = "[${env.JOB_NAME.replaceAll('%2F','/')}-${env.BUILD_NUMBER}] `${currentBuild.result}`" + "\n" +
+			"Elapsed Time: `${DURATION}` Mins" + "\n" +
+			"Build Branch: `${BUILD_BRANCH}`, Githash: `${GITHASH.take(7)}`" + "\n" +
+			"Display URL:" + "\n" +
+			"${env.RUN_DISPLAY_URL}"
+			if(currentBuild.result != "SUCCESS"){
+				slackSend channel: '#cloud_jenkins', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+			} else {
+				slackmsg = "${slackmsg}" + "\n" +
+				"Binary Download URL:" + "\n" +
+				"${UCLOUD_OSS_URL}/builds/pingcap/cloud-manager/${GITHASH}/centos7/tidb-cloud-manager.tar.gz"
+				slackSend channel: '#cloud_jenkins', color: 'good', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+			}
+		}
+	}
+}
+
+return this
---
+++ release_tidb_cloud_manager_binary_and_docker.groovy
@@ -0,0 +1,78 @@
+def call(TIDB_CLOUD_MANAGER_BRANCH, RELEASE_TAG) {
+	
+	def GITHASH
+	env.PATH = "${env.GOROOT}/bin:/bin:${env.PATH}"
+	def UCLOUD_OSS_URL = "http://pingcap-dev.hk.ufileos.com"
+
+	//define k8s pod template
+	podTemplate(
+		label: 'delivery',
+		volumes: [
+			hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
+		],
+		containers: [
+			containerTemplate(
+				name: 'build-env',
+				image: 'localhost:5000/pingcap/build_env:latest',
+				alwaysPullImage: true,
+				ttyEnabled: true,
+				command: 'cat')
+		]){
+		catchError {
+			node('delivery') {
+				def WORKSPACE = pwd()
+				stage('delivery cloud-manager binary') {
+					dir("${WORKSPACE}/cloud-manager"){
+						container('build-env') {
+							stage('Download tidb-cloud-manager binary'){
+								GITHASH = sh(returnStdout: true, script: "curl ${UCLOUD_OSS_URL}/refs/pingcap/cloud-manager/${TIDB_OPERATOR_BRANCH}/centos7/sha1").trim()
+								sh "curl ${UCLOUD_OSS_URL}/builds/pingcap/cloud-manager/${GITHASH}/centos7/tidb-cloud-manager.tar.gz | tar xz"
+							}
+
+							stage('Push tidb-cloud-manager Docker Image'){
+								sh """
+								mkdir -p tidb_cloud-manager_docker_build/bin
+								cd tidb_cloud-manager_docker_build
+								cp ../bin/* ./bin
+								cp ../docker/* .
+								cat > Dockerfile << __EOF__
+FROM alpine:3.5
+RUN apk add --no-cache ca-certificates
+COPY bin/tidb-cloud-manager /usr/local/bin/tidb-cloud-manager
+COPY pd.toml.tmpl /pd.toml.tmpl
+COPY tikv.toml.tmpl /tikv.toml.tmpl
+COPY apidocs /usr/local/apidocs
+ENTRYPOINT ["/usr/local/bin/tidb-cloud-manager"]
+__EOF__
+								cp -R /tmp/.docker ~/
+								docker build -t pingcap/tidb-cloud-manager:${RELEASE_TAG} .
+								docker push pingcap/tidb-cloud-manager:${RELEASE_TAG}
+								"""
+							}
+						}
+					}
+				}
+			}
+			currentBuild.result = "SUCCESS"
+		}
+		stage('Summary') {
+			echo("echo summary info ########")
+			def DURATION = (((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60) as double).round(2)
+			def slackmsg = "[${env.JOB_NAME.replaceAll('%2F','/')}-${env.BUILD_NUMBER}] `${currentBuild.result}`" + "\n" +
+			"Elapsed Time: `${DURATION}` Mins" + "\n" +
+			"tidb-cloud-manager Branch: `${TIDB_OPERATOR_BRANCH}`, Githash: `${GITHASH.take(7)}`" + "\n" +
+			"Display URL:" + "\n" +
+			"${env.RUN_DISPLAY_URL}"
+
+			if(currentBuild.result != "SUCCESS"){
+				slackSend channel: '#cloud_jenkins', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+			} else {
+				slackmsg = "${slackmsg}" + "\n" +
+				"tidb-cloud-manager Docker Image: `pingcap/tidb-cloud-manager:${RELEASE_TAG}`"
+				slackSend channel: '#cloud_jenkins', color: 'good', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+			}
+		}
+	}
+}
+
+return this
